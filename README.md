715642eed0285fe22e01ac8308a344fb70b1ab35
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2022-12-31 11:41:37 -0300

    Preparando o layout e os assets

---
+++ README.md
@@ -0,0 +1,33 @@
+# Intagram APP
+
+Aplicação clone do Instagram.
+
+## Tecnologias
+
+- Ruby 3.1.2
+- Rails 6.1.7
+- PostgreSQL
+
+## Inciando a aplicação
+
+- Intale as gems:
+
+```
+bundle install
+```
+
+- Criando o banco de dados e rodando as migrations:
+
+```
+rails db:create db:migrate
+```
+
+- Inicar o servidor local:
+
+```
+rails s
+```
+
+- Acesse no navegador:
+
+http://localhost:3000
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_url(@post), notice: "Post foi criado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+end
