fe8841f530f2b13f4b9d76ea548dc61aa4c0da23
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2023-01-02 16:59:26 -0300

    add flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,5 +9,12 @@ import "channels"
 
 import "../src/application.css"
 
+import "./flashMessages";
+// import "./modal";
+// import "./likes";
+
+// require("trix");
+// require("@rails/actiontext");
+
 Rails.start()
 ActiveStorage.start()
