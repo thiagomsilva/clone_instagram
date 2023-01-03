5040748e963a72ca5aaadea5d4f8664c52f6df53
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2023-01-02 21:50:26 -0300

    Melhorias layout página de autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "posts#index"
 
-  resources :posts, only: %i[ index show new create ]
+  devise_for :users, only: %i[sessions registrations passwords]
+
+  # resources :users, only: %i[show], param: :username
+  # get "edit_profile", to: "users#edit"
+  # patch "profile", to: "users#update"
+
+  resources :posts, only: %i[index show new create]
+  # resources :likes, only: %i[create destroy]
+  # resources :comments, only: %i[create]
 end
