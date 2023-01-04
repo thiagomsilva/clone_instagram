f6380ac5ef2bebd35cdd053d087a658b26dc92ae
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2023-01-04 20:03:40 -0300

    add editar perfil do usuário

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,21 +13,22 @@ class UsersController < ApplicationController
         render "posts/index"
     end
 
-    # def edit
-    # end
+    def edit
+    end
 
-    # def update
-    #     if current_user.update(user_params)
-    #     redirect_to root_path, notice: "Seu perfil foi atualizado com sucesso."
-    #     else
-    #     flash.now[:alert] = current_user.errors.full_messages.to_sentence
-    #     render :edit
-    #     end
-    # end
+    def update
+        # Para permiitir que somente o usuário logado alterem seu próprio user
+        if current_user.update(user_params)
+        redirect_to root_path, notice: "Seu perfil foi atualizado com sucesso."
+        else
+        flash.now[:alert] = current_user.errors.full_messages.to_sentence
+        render :edit
+        end
+    end
 
-    # private
+    private
 
-    # def user_params
-    #     params.require(:user).permit(:name, :avatar)
-    # end
+    def user_params
+        params.require(:user).permit(:name, :avatar)
+    end
 end  
